d775a7d7106ff87a70d3d45782a5c4226001620a
fersauce <carlifer.fernando@gmail.com>
2014-04-12 06:05:25 -0400

    Agregue modificar pass y varios elementos más.

--- principal/views.py
+++ principal/views.py
@@ -1,11 +1,14 @@
-from mercurial import context
-from django.core.context_processors import request
+import email
+from random import choice
+import string
+from django.contrib.auth.hashers import make_password, check_password
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http.response import HttpResponse, HttpResponseRedirect
-from principal.models import Receta, Comentario
+from django.template.loader import render_to_string
+from principal.models import Receta, Comentario, Usuario
 from django.contrib.auth.models import User
 from django.template.context import RequestContext
-from principal.forms import ContactoForm, RecetaForm, ComentarioForm
+from principal.forms import ContactoForm, RecetaForm, ComentarioForm, UsuarioForm, RecuperarPassForm, UserForm
 from django.core.mail.message import EmailMessage
 
 
@@ -16,22 +19,20 @@ def sobre(request):
 
 def inicio(request):
     recetas = Receta.objects.all()
-    return render_to_response('inicio.html', {'recetas': recetas})
+    return render_to_response('inicio.html', {'recetas': recetas}, context_instance=RequestContext(request))
 
 
 def usuarios(request):
     usuarios = User.objects.all()
     recetas = Receta.objects.all()
     return render_to_response('usuarios.html', {'usuarios': usuarios,
-                                                'recetas': recetas})
+                                                'recetas': recetas}, context_instance=RequestContext(request))
 
 
 def listaRecetas(request):
     recetas = Receta.objects.all()
     return render_to_response('recetas.html', {'datos': recetas},
                               context_instance=RequestContext(request))
-
-
 def detalleReceta(request, idReceta):
     dato = get_object_or_404(Receta, pk=idReceta)
     comentarios = Comentario.objects.filter(receta=dato)
@@ -50,10 +51,10 @@ def contacto(request):
             correo = EmailMessage(titulo, contenido,
                                   to=['carlifer.fernando@gmail.com'])
             correo.send()
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect('/', context_instance=RequestContext(request))
     else:
         formulario = ContactoForm()
-    return render_to_response('contactoform.html', {'formulario': formulario},
+    return render_to_response('comentarioform.html', {'formulario': formulario},
                               context_instance=RequestContext(request))
 
 
@@ -62,7 +63,7 @@ def nuevaReceta(request):
         formulario = RecetaForm(request.POST, request.FILES)
         if formulario.is_valid():
             formulario.save()
-            return HttpResponseRedirect('/recetas')
+            return HttpResponseRedirect('/recetas', context_instance=RequestContext(request))
     else:
         formulario = RecetaForm()
     return render_to_response('recetaform.html', {'formulario': formulario},
@@ -79,3 +80,60 @@ def nuevoComentario(request):
         formulario = ComentarioForm()
     return render_to_response('comentarioform.html', {'formulario': formulario},
                               context_instance=RequestContext(request))
+
+
+def usuario(request):
+    usuarios = User.objects.all()
+    return render_to_response('usuario.html', {'usuarios': usuarios}, context_instance=RequestContext(request))
+
+
+def usuarioNuevo(request):
+    if request.method == 'POST':
+        formulario = UserForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            '''user = Usuario.objects.create(username=formulario.cleaned_data['username'],
+                                          email=formulario.cleaned_data['email'],
+                                          password=make_password(formulario.cleaned_data['password']),
+                                          nombre=formulario.cleaned_data['nombre'])
+            contenido = render_to_string('mailing/bienvenida.html', {'usuario': user,
+                                                                     'pass': formulario.cleaned_data['password']})
+            correo = EmailMessage('Bienvenido a SS', contenido,
+                                  to=[formulario.cleaned_data['email']])
+            correo.content_subtype = "html"
+            correo.send()'''
+            formulario.save()
+            return HttpResponseRedirect('/usuario')
+    else:
+        formulario = UserForm()
+    return render_to_response('usuarioform.html', {'formulario': formulario},
+                              context_instance=RequestContext(request))
+
+
+def recuperarPass(request):
+    if request.method == 'POST':
+        formulario = RecuperarPassForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            password = generar_nuevo_pass(request, formulario.cleaned_data['correo'])
+            contenido = render_to_string('mailing/recuperacion_password.html', {'pass': password})
+            print str(contenido)
+            correo = EmailMessage('Restablecimiento de Pass de SS', contenido,
+                                  to=[formulario.cleaned_data['correo']])
+            correo.content_subtype = "html"
+
+            correo.send()
+            return HttpResponseRedirect('/usuario')
+    else:
+        formulario = RecuperarPassForm()
+    return render_to_response('recuperarpassform.html', {'formulario': formulario},
+                              context_instance=RequestContext(request))
+
+
+def generar_nuevo_pass(request, correo):
+    if correo is not None:
+        user = Usuario.objects.get(email=correo)
+        password = ''.join([choice(string.letters + string.digits) for i in range(10)])
+        user.password = make_password(password)
+        user.save()
+        return str(password)
+
+    return None
